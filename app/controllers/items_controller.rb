93c23f047d507b470f9aefc02134f8297c5cfebd
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-04 17:40:06 +0900

    商品情報のテーブルへの保存実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,19 @@ class ItemsController < ApplicationController
   end
   
   def create
-    @item = Item.create
+    @item = Item.new(item_params)
+    binding.pry
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :burden_id, :area_id, :days_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :days
+
+
+  with_options presence: true do
+    validates :name, :description, :price
+  end
+  with_options numericality: {other_than: 0} do
+    validates :category_id, :status_id, :burden_id, :area_id, :days_id
+  end
+
 end

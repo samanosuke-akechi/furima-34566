b7d5ce9d272bf765e85bc433140e06852810ea83
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-04 18:43:18 +0900

    価格のバリデーション300~9999999の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,8 @@ class Item < ApplicationRecord
 
 
   with_options presence: true do
-    validates :name, :description, :price
+    validates :name, :description, :image
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is invalid'}
   end
   with_options numericality: {other_than: 0} do
     validates :category_id, :status_id, :burden_id, :area_id, :days_id

bf4b70d7ed0a64a1050a846c08a6c2278b0b8540
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-04 19:26:20 +0900

    エラーメッセージの文言追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
     validates :name, :description, :image
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is invalid'}
   end
-  with_options numericality: {other_than: 0} do
+  with_options numericality: {other_than: 0, message: "Select"} do
     validates :category_id, :status_id, :burden_id, :area_id, :days_id
   end
 

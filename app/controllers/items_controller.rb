8bb4cb4f3d6e36e06450077532c42b9cd619eb9f
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-06 13:50:23 +0900

    リファクタリング（商品情報取得のコード）
    set_itemメソッドを定義してその処理としてidに紐づく商品情報の取得を行う。
    before_actionで商品詳細表示・編集・更新時にメソッドが呼び出される。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -20,18 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params) # updateというActiveRecordメソッドを使用
       redirect_to action: :show
     else
@@ -43,5 +41,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :burden_id, :area_id, :days_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end

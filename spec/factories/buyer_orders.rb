2afc4fd2d0b992a5cc3316cd96e771f61e6df2b0
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-09 21:59:06 +0900

    商品購入機能モデル単体テストコード

---
+++ spec/factories/buyer_orders.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :buyer_order do
+    token       {'tok_abcdefghijk00000000000000000'}
+    postal_code {'123-4567'}
+    area_id     {'1'}
+    city        {'札幌市'}
+    address     {'1-1'}
+    building    {'札幌ビル'}
+    phone_num   {'09012345678'}
+  end
+end

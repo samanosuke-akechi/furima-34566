1d5f5606653e0f8fba1b8f431a9f664bb9853356
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-09 11:28:54 +0900

    buyersコントローラー　保存処理・条件分岐・ストロングパラメーター定義
    indexアクションにはエラーメッセージのビューに渡すインスタンスを定義。
    createアクションにはバリデーションを通過した場合に保存してトップページに遷移する条件分岐を定義。失敗時は購入画面に止まる。renderでビューを返した際に商品情報を表示できるようインスタンスを定義。

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,5 +1,23 @@
 class BuyersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @buyer_order = BuyerOrder.new
+  end
+
+  def create
+    @buyer_order = BuyerOrder.new(buyer_params)
+    if @buyer_order.valid?
+      @buyer_order.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def buyer_params
+    params.permit(:item_id, :postal_code, :area_id, :city, :address, :building, :phone_num).merge(user_id: current_user.id)
   end
 end

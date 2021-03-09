20827a7e141ea37904df1ed3a2b45f320b38357d
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-09 12:43:45 +0900

    クレジットカード決済機能
    公開鍵と秘密鍵はcommit用に仮のものを置いている。
    実際の公開鍵と秘密鍵にて動作確認済み。

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,16 +1,17 @@
 class BuyersController < ApplicationController
   def index
-    @item = Item.find(params[:item_id])
     @buyer_order = BuyerOrder.new
+    item_info
   end
 
   def create
     @buyer_order = BuyerOrder.new(buyer_params)
     if @buyer_order.valid?
+      pay_item
       @buyer_order.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
+      item_info
       render :index
     end
   end
@@ -18,6 +19,19 @@ class BuyersController < ApplicationController
   private
 
   def buyer_params
-    params.permit(:item_id, :postal_code, :area_id, :city, :address, :building, :phone_num).merge(user_id: current_user.id)
+    params.permit(:item_id, :postal_code, :area_id, :city, :address, :building, :phone_num).merge(user_id: current_user.id, token: params[:token])
+  end
+
+  def item_info
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_***********"
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
--- app/models/buyer_order.rb
+++ app/models/buyer_order.rb
@@ -1,12 +1,12 @@
 class BuyerOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :area_id, :city, :address, :building, :phone_num, :buyer_id, :user_id, :item_id
+  attr_accessor :postal_code, :area_id, :city, :address, :building, :phone_num, :buyer_id, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :area_id, numericality: {other_than: 0, message: "Select"}
     validates :phone_num, format: {with: /\d{10,11}/, message: "is invalid. Do not include(-)"}
-    validates :city, :address
+    validates :city, :address, :token
   end
 
   def save

7cc0662659d365d74467eeb991019ae77e43ebf1
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-09 21:51:15 +0900

    電話番号のバリデーション修正

--- app/models/buyer_order.rb
+++ app/models/buyer_order.rb
@@ -5,7 +5,7 @@ class BuyerOrder
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :area_id, numericality: {other_than: 0, message: "Select"}
-    validates :phone_num, format: {with: /\d{10,11}/, message: "is invalid. Do not include(-)"}
+    validates :phone_num, format: {with: /\A\d{10,11}\z/, message: "is invalid. Do not include(-)"}
     validates :city, :address, :token
   end
 

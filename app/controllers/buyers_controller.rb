de8c2794d8f6872b03df3f0eb3b502bee823cae1
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-09 22:17:10 +0900

    環境変数設定

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -31,7 +31,7 @@ class BuyersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = ""
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,  # 商品の値段
       card: params[:token],    # カードトークン

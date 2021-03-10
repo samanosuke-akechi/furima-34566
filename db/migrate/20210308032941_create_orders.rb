1755b7b1c57f8e54a350bf3e903ef94b0e004fb3
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-10 11:30:02 +0900

    ordersテーブルのマイグレーションファイル修正

--- db/migrate/20210308032941_create_orders.rb
+++ db/migrate/20210308032941_create_orders.rb
@@ -5,7 +5,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       t.integer    :area_id,     null: false
       t.string     :city,        null: false
       t.string     :address,     null: false
-      t.string     :building,    null: false
+      t.string     :building
       t.string     :phone_num,   null: false
       t.references :buyer,       foreign_key: true
       t.timestamps

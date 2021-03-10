99df0116b8a79a03da644c119113bee3c88c5ad7
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-03-10 11:28:31 +0900

    バリデーションのカラム追加、attr_accessorの引数修正

--- app/models/buyer_order.rb
+++ app/models/buyer_order.rb
@@ -1,12 +1,12 @@
 class BuyerOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :area_id, :city, :address, :building, :phone_num, :buyer_id, :user_id, :item_id, :token
+  attr_accessor :postal_code, :area_id, :city, :address, :building, :phone_num, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :area_id, numericality: {other_than: 0, message: "Select"}
     validates :phone_num, format: {with: /\A\d{10,11}\z/, message: "is invalid. Do not include(-)"}
-    validates :city, :address, :token
+    validates :city, :address, :token, :user_id, :item_id  # Formオブジェクトではアソシエーションを組めないので、外部キーについてもpresence: trueが必要
   end
 
   def save
